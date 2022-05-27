06bb45724b6e6ae9ac1b7d443b95c0ac5ee191a4
唤雨考拉 <therainisme@qq.com>
2022-05-27 16:55:43 +0800

    feat: add dark mode style & SEO

---
+++ components/MyHead.tsx
@@ -0,0 +1,22 @@
+import Head from "next/head";
+
+type Props = {
+    title: string;
+    meta?: { name: string, content: string; }[];
+};
+
+export default function MyHead(props: Props) {
+    return (
+        <Head>
+            <title>{props.title}</title>
+            <meta name="description" content="这里写着的肯定是一堆废话，如果我这里不写废话的话，那么废话就没有地方可写了。我真的一点话都编不出来了，谁来救救我告诉我这里应该写什么比较好。" />
+            <meta name="keywords" content="blog,notebook,Therainisme,Algorithm,vivy,题解,算法,编程,笔记,学习,react" />
+            <meta name="author" content="therainisme" />
+            <meta name="google-site-verification" content="rzQXcUt4ASeLCPCefMkF6oo7r5JNemgtLF_b4o_ar34" />
+            {props.meta?.map((meta) => (
+                <meta key={meta.name} name={meta.name} content={meta.content} />
+            ))}
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+    );
+}
